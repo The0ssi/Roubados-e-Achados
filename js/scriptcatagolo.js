7e3c20aec6e4a68e5cf5aaea42564a2e3ee87898
Isabelly Victória <isabelly.v.silva12@aluno.senai.br>
2025-11-03 16:20:33 -0300

    Update scriptcatagolo.js

--- js/scriptcatagolo.js
+++ js/scriptcatagolo.js
@@ -1,46 +1,19 @@
-function aplicarFiltros() {
-      const categorias = Array.from(document.querySelectorAll('.filtro-categoria:checked')).map(c => c.value);
-      const locais = Array.from(document.querySelectorAll('.filtro-local:checked')).map(l => l.value);
-      const dataFiltro = document.getElementById('filtro-data').value;
+// FUNCTION PARA O TAB - (muda entre achados e perdidos)
+function changeTab(tab) {
+  const line = document.querySelector('.line');
+  const tabs = document.querySelectorAll('.tab');
 
-      document.querySelectorAll('.item').forEach(item => {
-        const categoria = item.dataset.categoria;
-        const local = item.dataset.local;
-        const data = item.dataset.data;
+  if (tab === 'achado') {
+    line.style.left = '0';
+    line.style.width = '50%';
+  } else if (tab === 'perdido') {
+    line.style.left = '50%';
+    line.style.width = '50%';
+  }
 
-        let mostrar = true;
+  const formulario = document.getElementById('formulario');
+  formulario.classList.remove('achado', 'perdido');
+  formulario.classList.add(tab);
 
-        if (categorias.length > 0 && !categorias.includes(categoria)) {
-          mostrar = false;
-        }
-        if (locais.length > 0 && !locais.includes(local)) {
-          mostrar = false;
-        }
-        if (dataFiltro && data !== dataFiltro) {
-          mostrar = false;
-        }
-
-        item.style.display = mostrar ? 'block' : 'none';
-      });
-    }
-
-
-
-
-
-document.querySelectorAll('.pagination .page-link').forEach(page => {
-  page.addEventListener('click', function(event) {
-    event.preventDefault();
-    
-    
-    document.querySelectorAll('.pagination .page-item').forEach(item => {
-      item.classList.remove('active');
-    });
-
-    this.parentElement.classList.add('active');
-
-    let pageNumber = this.textContent; 
-    console.log('Página selecionada:', pageNumber);
-    
-  });
-});
+  console.log("A aba selecionada é: " + tab);
+}
