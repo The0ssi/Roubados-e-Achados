2bf80f6fbec460a17836d1df5a20e017087844fb
Beatriz Regina <beatriz.r.ferreira@aluno.senai.br>
2025-11-04 15:05:28 -0300

    Update and rename scriptcatagolo.js to scriptCatagolo.js

---
+++ js/scriptCatagolo.js
@@ -0,0 +1,25 @@
+function changeTab(tab) {
+  const line = document.querySelector('.line');
+  const tabs = document.querySelectorAll('.tab');
+
+  if (tab === 'achado') {
+    line.style.left = '0';
+    line.style.width = '50%';
+  } else if (tab === 'perdido') {
+    line.style.left = '50%';
+    line.style.width = '50%';
+  }
+
+  const returnText = document.querySelector('.return-text');
+  const title = returnText.querySelector('h2');
+  const paragraph = returnText.querySelector('p');
+
+  if (tab === 'achado') {
+    title.textContent = 'Catálogo dos itens achados';
+    paragraph.textContent = 'Catálogo de achados — seu item pode estar aqui.';
+  } else if (tab === 'perdido') {
+    title.textContent = 'Catálogo dos itens perdidos';
+    paragraph.textContent = 'Catálogo de perdidos — o item pode estar aqui.';
+  }
+  console.log("A aba selecionada é: " + tab);
+}
