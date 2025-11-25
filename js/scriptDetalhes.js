de64f62f0ffa350e280de7edfb5f0019b6ebba0b
Beatriz Regina <beatriz.r.ferreira@aluno.senai.br>
2025-11-25 11:03:19 -0300

    Update scriptDetalhes.js

--- js/scriptDetalhes.js
+++ js/scriptDetalhes.js
@@ -1,25 +1,41 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const isLoggedIn = sessionStorage.getItem("isLoggedIn");
-    const isAdmin = sessionStorage.getItem("isAdmin") === "true"; 
+function changeTab(tab) {
+  const line = document.querySelector('.line');
+  const tabs = document.querySelectorAll('.tab');
 
-    // Seleciona o botão pela classe
-    const achadoButton = document.querySelector(".btn-registrar");
+  if (tab === 'achado') {
+    line.style.left = '0';
+    line.style.width = '50%';
+  } else if (tab === 'perdido') {
+    line.style.left = '50%';
+    line.style.width = '50%';
+  }
 
-    if (!achadoButton) {
-        console.warn("Botão 'Achado' não encontrado no DOM.");
-        return; // evita erros
+  const returnText = document.querySelector('.return-text');
+  const title = returnText.querySelector('h2');
+  const paragraph = returnText.querySelector('p');
+
+  if (tab === 'achado') {
+    title.textContent = 'Catálogo dos itens achados';
+    paragraph.textContent = 'Catálogo de achados — seu item pode estar aqui.';
+  } else if (tab === 'perdido') {
+    title.textContent = 'Catálogo dos itens perdidos';
+    paragraph.textContent = 'Catálogo de perdidos — o item pode estar aqui.';
+  }
+  console.log("A aba selecionada é: " + tab);
+}
+
+
+function toggleMenuFiltros() {
+    const menu = document.getElementById("menu-filtros-mobile");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+
+// Clonar conteúdo do aside (filtros completos) e jogar dentro do menu mobile
+document.addEventListener("DOMContentLoaded", () => {
+    const asideFiltros = document.querySelector(".filtro-container");
+    const destino = document.querySelector(".filtros-completos");
+
+    if (asideFiltros && destino) {
+        destino.innerHTML = asideFiltros.innerHTML;
     }
-
-    // Caso não esteja logado → opcional redirecionar
-    if (!isLoggedIn) {
-        console.warn("Usuário não está logado!");
-        // window.location.href = "login.html";
-    }
-
-    // SE FOR ALUNO → esconde o botão
-    if (!isAdmin) {
-        achadoButton.style.display = "none";
-    }
-
-    // SE FOR SECRETARIA → mantém visível
 });
